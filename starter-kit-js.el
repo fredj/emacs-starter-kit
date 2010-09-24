8c9d9828384bca15023dc7382e1fb979a366a44b
fredj <frederic.junod@camptocamp.com>
2010-09-24 09:08:12 +0200

    remove function -> f

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -17,11 +17,7 @@
           (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
           ;; fixes problem with pretty function font-lock
           (define-key espresso-mode-map (kbd ",") 'self-insert-command)
-          (font-lock-add-keywords
-           'espresso-mode `(("\\(function *\\)("
-                             (0 (progn (compose-region (match-beginning 1)
-                                                       (match-end 1) "ƒ")
-                                       nil)))))))
+          ))
 
 (provide 'starter-kit-js)
 ;;; starter-kit-js.el ends here
