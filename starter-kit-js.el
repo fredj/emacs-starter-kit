b46d3344d18bc63339a4ba37c71be5824e51f1b4
fredj <frederic.junod@camptocamp.com>
2010-10-14 08:22:26 +0200

    deactivate paredit (for '{' and '}') and moz-minor-mode

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -5,19 +5,8 @@
 (autoload 'espresso-mode "espresso" "Start espresso-mode" t)
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
-(add-hook 'espresso-mode-hook 'moz-minor-mode)
 (add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
 (add-hook 'espresso-mode-hook 'run-coding-hook)
-
-;; If you prefer js2-mode, use this instead:
-;; (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
-
-(eval-after-load 'espresso
-  '(progn (define-key espresso-mode-map "{" 'paredit-open-curly)
-          (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
-          ;; fixes problem with pretty function font-lock
-          (define-key espresso-mode-map (kbd ",") 'self-insert-command)
-          ))
 
 (provide 'starter-kit-js)
 ;;; starter-kit-js.el ends here
