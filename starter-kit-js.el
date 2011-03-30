aac9537e4f433350f016d3aa4cc1a23cbf60c410
fredj <frederic.junod@camptocamp.com>
2011-03-30 11:41:43 +0200

    flymake-display-err-menu-for-current-line without X (from https://github.com/avar/dotemacs/blob/master/.emacs)

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -8,6 +8,22 @@
 (add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
 (add-hook 'espresso-mode-hook 'run-coding-hook)
 
+(eval-after-load "flymake"
+  '(defun flymake-display-err-menu-for-current-line ()
+    "Display a message with errors/warnings for current line if it
+has errors and/or warnings."
+    (interactive)
+    (let* ((line-no             (flymake-current-line-no))
+           (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
+           (menu-data           (flymake-make-err-menu-data line-no line-err-info-list)))
+      (if menu-data
+          (let ((messages))
+            (push (concat (car menu-data) ":") messages)
+            (dolist (error-or-warning (cadr menu-data))
+              (push (car error-or-warning) messages))
+            (message "%s" (mapconcat #'identity (reverse messages) "\n")))))))
+
+
 (require 'closure-lint-mode)
 
 (provide 'starter-kit-js)
